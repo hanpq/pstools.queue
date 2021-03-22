e3f891063952283643689c3642e3a51a15bd4c79
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2021-03-22 21:01:42 +0100

    updated test file path

--- tests/unit/Initialize-Queue.Tests.ps1
+++ tests/unit/Initialize-Queue.Tests.ps1
@@ -1,42 +1,43 @@
 BeforeAll {
-    . (Resolve-Path -Path "$PSScriptRoot\..\..\Source\private\class.extendedqueue.ps1")
-    . (Resolve-Path -Path "$PSScriptRoot\..\..\Source\public\Initialize-Queue.ps1")
+    . (Resolve-Path -Path "$PSScriptRoot\..\..\source\private\class.extendedqueue.ps1")
+    . (Resolve-Path -Path "$PSScriptRoot\..\..\source\public\Initialize-Queue.ps1")
 }
 
-Describe -Name "Initialize-Queue.ps1" -Fixture {
+Describe -Name 'Initialize-Queue.ps1' -Fixture {
     Context -Name 'When calling Initialize-Queue' {
         BeforeAll {
             $Queue = Initialize-Queue
         }
         It -Name 'Should return a object of type [ExtendedQueue]' {
-            $Queue.GetType().Name | should -Be "ExtendedQueue"
+            $Queue.GetType().Name | Should -Be 'ExtendedQueue'
         }
         It -Name 'Should contain a [System.Collections.Queue] object in property queue' {
-            ,($Queue.Queue) | should -beoftype [System.Collections.Queue]
+            , ($Queue.Queue) | Should -BeOfType [System.Collections.Queue]
         }
         It -Name 'Should contain a [system.collections.Generic.Queue[pscustomobject]]' {
-            , ($Queue.LastOperations) | should -beoftype [system.collections.Generic.Queue[pscustomobject]]
+            , ($Queue.LastOperations) | Should -BeOfType [system.collections.Generic.Queue[pscustomobject]]
         }
         It -Name 'Should have default values for properties' {
-            $Queue.TotalItemsAdded | should -be 0
-            $Queue.TotalItemsRemoved | should -be 0
-            $Queue.AddPerSec | should -be 0
-            $Queue.RemovePerSec | should -be 0
-            $Queue.Velocity | should -be 0
-            $Queue.PerformanceHistory | should -be 50
+            $Queue.TotalItemsAdded | Should -Be 0
+            $Queue.TotalItemsRemoved | Should -Be 0
+            $Queue.AddPerSec | Should -Be 0
+            $Queue.RemovePerSec | Should -Be 0
+            $Queue.Velocity | Should -Be 0
+            $Queue.PerformanceHistory | Should -Be 50
         }
         It -Name 'Should have correct type of properties' {
-            $Queue.TotalItemsAdded | should -beoftype [int64]
-            $Queue.TotalItemsRemoved | should -beoftype [int64]
-            $Queue.AddPerSec | should -beoftype [double]
-            $Queue.RemovePerSec | should -beoftype [double]
-            $Queue.Velocity | should -beoftype [double]
-            $Queue.PerformanceHistory | should -beoftype [int]
+            $Queue.TotalItemsAdded | Should -BeOfType [int64]
+            $Queue.TotalItemsRemoved | Should -BeOfType [int64]
+            $Queue.AddPerSec | Should -BeOfType [double]
+            $Queue.RemovePerSec | Should -BeOfType [double]
+            $Queue.Velocity | Should -BeOfType [double]
+            $Queue.PerformanceHistory | Should -BeOfType [int]
         }
         It -Name 'Should have methods' {
-            $Methods = @('AddQueueItem','GetNextQueueItem','ShowNextQueueItem','GetQueueCount','ClearAllQueueItems','GetAllQueueItems','RotateLastOperations','CalculateSpeed')
-            foreach ($Method in $Methods) {
-                $Queue.PSObject.Members.Name | should -contain $Method
+            $Methods = @('AddQueueItem', 'GetNextQueueItem', 'ShowNextQueueItem', 'GetQueueCount', 'ClearAllQueueItems', 'GetAllQueueItems', 'RotateLastOperations', 'CalculateSpeed')
+            foreach ($Method in $Methods)
+            {
+                $Queue.PSObject.Members.Name | Should -Contain $Method
             }
         }
     }
